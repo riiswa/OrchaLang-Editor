c6fdc4295f6d15f312441f2275aa1e37009c34ac
riiswa <wradji64@gmail.com>
2019-12-06 22:07:16 +0100

    Decentralize data

--- src/app/code.service.ts
+++ src/app/code.service.ts
@@ -1,22 +1,10 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
-import {WriteComponent} from './write/write.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeService {
   content: string;
-
   constructor() {
-  }
-
-  getContent(): Observable<string> {
-    return of(this.content);
-  }
-
-
-  setContent(content) {
-    this.content = content;
   }
 }
--- src/app/write/write.component.ts
+++ src/app/write/write.component.ts
@@ -11,28 +11,20 @@ import {CookieService} from 'ngx-cookie-service';
   styleUrls: ['./write.component.css']
 })
 export class WriteComponent implements OnInit {
-  content: string;
   cookieName: 'orchalang_editor_content'
 
   constructor(private codeService: CodeService, private cookieService: CookieService) {}
 
   ngOnInit() {
-    this.getContent();
     if (this.cookieService.check(this.cookieName)) {
-      this.content = this.cookieService.get(this.cookieName);
+      this.codeService.content = this.cookieService.get(this.cookieName);
     } else {
-      this.cookieService.set(this.cookieName, this.content);
+      this.cookieService.set(this.cookieName, this.codeService.content);
     }
   }
 
-  getContent(): void {
-    this.codeService.getContent().subscribe(content => this.content = content);
-  }
-
   onChange() {
-    this.codeService.setContent(this.content);
-    this.cookieService.set(this.cookieName, this.content);
-    console.log(this.content);
+    this.cookieService.set(this.cookieName, this.codeService.content);
   }
 
 }
