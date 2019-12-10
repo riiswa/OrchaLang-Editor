586ef7f29f3ec45cbfc9cbde664cf02fd293b05a
RemiDrt <rd.duret@hotmail.fr>
2019-12-10 14:54:56 +0100

    adding console with ng-erminal

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { NgTerminal } from 'ng-terminal';
 
 @Component({
   selector: 'app-console',
   templateUrl: './console.component.html',
   styleUrls: ['./console.component.css']
 })
-export class ConsoleComponent implements OnInit {
-
+export class ConsoleComponent implements OnInit, AfterViewInit {
+  @ViewChild('term', { static: true }) child: NgTerminal;
   constructor() { }
+  ngAfterViewInit() {
+
+  }
+
 
   ngOnInit() {
   }
