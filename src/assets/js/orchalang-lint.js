1180622d2c14fefaf3ecef4f88afcf6bcb01cbbd
riiswa <wradji64@gmail.com>
2019-12-10 16:49:43 +0100

    Firefox compatibily & change architecture of syntax analysis

--- src/assets/js/orchalang-lint.js
+++ src/assets/js/orchalang-lint.js
@@ -15,80 +15,55 @@
     return str.replace(/\s+/g, ' ').trim();
   }
 
-  function createAlertObject(message, severity, from, to, line) {
+  function generateError(line, lineNumber) {
     return {
-      message: message,
-      severity: severity,
-      from: {line: line, ch: to, sticky: null},
-      to: {line: line, ch: from, sticky: null}
+      message: "Syntax error at instruction: " + line,
+      severity: "error",
+      from: {line: lineNumber, ch: 0, sticky: null},
+      to: {line: lineNumber, ch: line.length, sticky: null}
     }
   }
 
-  function sendInstructionValidation(str) {
-    const sendSyntax = /^send ?(?<data>.*?)?(\.(?<variables>.*))? to ?(?<destinations>.*?)$/;
-    const sendSyntaxMessage = 'send <event>.result to <destination event handler>';
-    if (sendSyntax.test(str)) {
-
-      var missingPart = [];
-
-      const groups = sendSyntax.exec(str).groups;
-
-      if (!groups.data) missingPart.push('data');
-      if (!groups.destinations) missingPart.push('destinations');
-      if (!groups.variables) missingPart.push('variables');
-
-      if (missingPart.length !== 0) {
-        return {
-          message: 'This instruction have missing field(s): ' + missingPart.join(', ') + '\n' + 'The send instruction syntax should be: ' + sendSyntaxMessage,
-          severity: 'warning'
-        }
-
-      } else return {message: null, severity: null}
-
-    } else {
-      return {
-        message: 'This instruction is incorrect.\n The send instruction syntax should be: ' + sendSyntaxMessage,
-        severity: 'error'
-      }
-    }
+  function analysis(syntax, line, lineNumber) {
+    const cleanLine = cleanString(line);
+    if (!syntax.test(cleanLine)) return generateError(cleanLine, lineNumber)
   }
-
-  function versionInstructionValidation(str) {
-    const versionSyntax = /^version(\s*):(\s*)(?<version>.*?)$/;
-
-    if (versionSyntax.test(str)) return {message: null, severity: null};
-    else return {
-      message: 'This instruction is incorrect.\n The version instruction syntax should be version: xx',
-      severity: 'error'
-    }
-  }
-
 
   function validator(text, options) {
+
+    const sendSyntax = /send (.*?)(\.(.*))? to (.*?)/;
+    const versionSyntax = /version(\s*):(\s*)(.*?)/;
+    const domainSyntax = /domain(\s*):(\s*)(.*?)/;
+    const titleSyntax = /title(\s*):(\s*)(.*?)/;
+    const authorsSyntax = /authors(\s*):(\s*)(.*?)/;
+    const descriptionSyntax = /description(\s*):(\s*)(.*?)/;
+    const computeSyntax = /compute (.*?)( with (.*))?/;
+    const receiveSyntax = /receive (.*?) from (\w+)( condition (.*))?/;
+
+    const errors = [];
     const lines = text.split('\n');
-
-    var result = [];
-
-    for (const [i, line] of lines.entries()) {
+    for (const [lineNumber, line] of lines.entries()) {
       const cleanLine = cleanString(line);
-      if (cleanLine === '') {
+      if (cleanLine === '' || cleanLine.startsWith('//')) {
       } else if (cleanLine.startsWith('send')) {
-        const validation = sendInstructionValidation(cleanLine);
-        if (validation.message) {
-          result.push(createAlertObject(validation.message, validation.severity, 0, line.length, i))
-        }
+        errors.push(analysis(sendSyntax, line, lineNumber))
       } else if (cleanLine.startsWith('version')) {
-        const validation = versionInstructionValidation(cleanLine);
-        if (validation.message) {
-          result.push(createAlertObject(validation.message, validation.severity, 0, line.length, i))
-        }
-      } else {
-        result.push(createAlertObject('This instruction don\'t exist', 'error', 0, line.length, i))
-      }
+        errors.push(analysis(versionSyntax, line, lineNumber))
+      } else if (cleanLine.startsWith('domain')) {
+        errors.push(analysis(domainSyntax, line, lineNumber))
+      } else if (cleanLine.startsWith('title')) {
+        errors.push(analysis(titleSyntax, line, lineNumber))
+      } else if (cleanLine.startsWith('description')) {
+        errors.push(analysis(descriptionSyntax, line, lineNumber))
+      } else if (cleanLine.startsWith('compute')) {
+        errors.push(analysis(computeSyntax, line, lineNumber))
+      } else if (cleanLine.startsWith('receive')) {
+        errors.push(analysis(receiveSyntax, line, lineNumber))
+      } else if (cleanLine.startsWith('authors')) {
+        errors.push(analysis(authorsSyntax, line, lineNumber))
+      } else errors.push(generateError(cleanLine, lineNumber))
     }
-
-    return result
-
+    return errors.filter(o => o)
   }
 
   CodeMirror.registerHelper("lint", "orchalang", validator);
